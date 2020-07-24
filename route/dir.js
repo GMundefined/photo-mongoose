9418cb43f9c24ab2f3f6b367f14ece326f89794d
GM_undefined <1636960567@qq.com>
2020-07-24 09:06:55 +0800

    day02

--- route/dir.js
+++ route/dir.js
@@ -2,6 +2,7 @@
 const express=require('express')
 const router =express.Router();
 const file =require('../model/file.js')
+const {SUCCESS , FAILED}=require('../status.js')
 //处理dir请求 显示服务器上所有的相册
 router.get('/',function(req,res){
   //将uploads里面的文件夹显示出来
@@ -12,13 +13,85 @@ router.get('/',function(req,res){
       return ;
     }
     // res.send(files)  数据已经拿到
+    res.render('index',{dirs:files})
   });
+});
 
-}) 
+//处理get方式的 /dir/mkdir请求 跳转到新建相册页面
+router.get('/mkdir',function(req,res){
+  //跳转到创建页面
+  //该页面不需要渲染数据 所以不需要传递数据过去
+  res.render('create');
+})
+//处理post方式的 /dir/mkdir请求 创建相册 ajax请求
+router.post('/mkdir',function(req,res){
+  //获取请求参数dirName app.js已经设置过解析方式
+  var dirName=req.body.dirName;
+  //检查dirName的合法性
+  if(!dirName){
+    res.send({status:FAILED,msg:"相册名不合法"})
+    return ;
+  }
+  //调用file.js暴露的create方法来创建文件夹
+  dirName='uploads/'+dirName;//将文件夹创建在uploads文件夹里面
+  file.create(dirName,function(err){
+    if(err){
+      console.log(err);
+      res.send({status:FAILED,msg:'创建失败'});
+      return ;
+    }
+    res.send({status:SUCCESS,msg:'创建成功'});
+  });
+})
+//处理get方式的 /dir/check请求 ajax请求 获取传递过来的参数 并检查文件夹名称是否存在
+router.get('/check',function(req,res){
+  //获取参数
+  var dirName =req.query.dirName;
+  if(!dirName){//如果传递过来的是空字符串  返回状态1 
+    res.send({status:FAILED,msg:"文件夹名称不能为空"})
+    return ;
+  }
+  //获取所有文件夹 检查其中有没有该名称的文件夹
+  file.getDirs('./uploads',function(err,files){
+    if(err){
+      console.log(err);
+      res.send({status:FAILED,msg:"服务器网络故障,稍后再试"})
+      return ;
+    }
+    //读取成功 开始检查是否存在
+    var result= files.find(function(val){
+      return val===dirName;
+    })
+    if(result){
+      //说明存在这个文件夹
+      res.send({status:FAILED,msg:"相册已存在"})
+      return ;
+    }
+    //没找到 说明文件夹名字可以使用
+    res.send({status:SUCCESS,msg:"名称可以使用"})
+  })
 
+})
 
-
-
+//处理/dir/delete请求 删除相册
+router.get('/delete',function(req,res){
+  //获取参数
+  var dirName =req.query.dirName.trim();
+  if(!dirName){//参数不合法
+    res.send({status:FAILED,msg:'参数不合法'})
+    return ;
+  }
+  //参数合法 调用file的delete方法 删除文件夹
+  dirName ='uploads/'+dirName;
+  file.delete(dirName,function(err){
+    if(err){
+      console.log(err)
+      res.send({status:FAILED,msg:'删除失败'})
+      return ;
+    }
+    res.send({status:SUCCESS,msg:'删除成功'})
+  })
+})
 
 
 
--- route/pic.js
+++ route/pic.js
@@ -1,10 +1,79 @@
 //创建图片相关的路由
-const express=require('express')
+const express=require('express');
+const {SUCCESS , FAILED}=require('../status.js')
 const router =express.Router();
+const file =require('../model/file.js')
+const fd=require('formidable')
+//处理 /pic/show 请求 展示某个相册的内容
+router.get('/show',function(req,res){
+  //获取请求参数 得到被点击的相册名称
+  var dir=req.query.dirName.trim();
+  if(!dir){  
+    res.render('error',{errMsg:"获取相册出错"})
+    return ;
+  }
+  //调用file里面的getDirs方法 获取文件夹中的内容
+  dirName='uploads/'+dir;
+  file.getDirs(dirName,function(err,files){
+    if(err){//读取错误
+      console.log(err);
+      res.render('error',{errMsg:"读取相册内容失败"});
+      return ;
+    }
+    //读取成功
+    res.render('show.ejs',{files:files,dirName:dir});
+  }) 
+})
 
 
+//处理get方式传递过来的 /pic/upload 请求 跳转至上传页面
+router.get('/upload',function(req,res){
+  //在上传图片时  需要知道将图片传到哪个相册中
+  //获取uploads下所有的相册名
+  file.getDirs('./uploads',function(err,dirs){
+    if(err){
+      console.log(err);
+      res.render('error',{errMsg:"获取相册出错"});
+      return ;
+    }
+    //获取到相册 将其传递给上传页面
+    res.render('upload',{dirs:dirs});
+  })
+})
 
+//处理post 方式的/pic/upload请求 上传图片
+router.post('/upload',function(req,res){
+  //处理图片的上传
+  //创建对象
+  // var form=fd();
+  var form=new fd.IncomingForm();
+  //设置上传临时保持路径
+  form.uploadDir='./temp';
+  form.parse(req,function(err,fields,files){//解析数据
+    if(err){
+      console.log(err)
+      res.render('error',{errMsg:'上传图片失败'})
+      return ;
+    }
+    //获取表单中的数据
+    //文本域数据:选择的文件夹名称
+    var dirName=fields.dirName;
+    //获取图片对象
+    var pic=files.pic;
+    //调用file.js暴露的rename方法处理图片 修改图片名称 和替换存储路径
+    file.rename(dirName,pic,function(err){
+      if(err){
+        console.log(err)
+        res.render('error',{errMsg:'图片上传失败'})
+        return ;
+      }
+      //上传成功 跳转到上传图片的文件夹中 显示图片
+      res.redirect('/pic/show?dirName='+dirName);//重定向
 
+    });
+  })
+
+})
 
 
 
